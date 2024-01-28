e20370f451810578ef279911a51559a8f3ecfe67
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-01-28 20:12:22 +0300

    Minor View Controller
    - Added MinorViewController to demonstrate entire Life Cycle of the ViewController

--- LifeCycleVC/LifeCycleVC/Screens/Minor/MinorViewController.swift
+++ LifeCycleVC/LifeCycleVC/Screens/Minor/MinorViewController.swift
@@ -1,53 +1,53 @@
 //
-//  MainViewController.swift
+//  MinorViewController.swift
 //  LifeCycleVC
 //
-//  Created by Stanislav Lemeshaev on 27.01.2024.
+//  Created by Stanislav Lemeshaev on 28.01.2024.
 //  Copyright © 2024 slemeshaev. All rights reserved.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MinorViewController: UIViewController {
     
     override func loadView() {
         super.loadView()
-        print("### MainViewController")
+        print("*** MinorViewController")
         print(#function)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("### MainViewController")
+        print("*** MinorViewController")
         print(#function)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("### MainViewController")
+        print("*** MinorViewController")
         print(#function)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("### MainViewController")
+        print("*** MinorViewController")
         print(#function)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("### MainViewController")
+        print("*** MinorViewController")
         print(#function)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("### MainViewController")
+        print("*** MinorViewController")
         print(#function)
     }
     
     deinit {
-        print("### MainViewController")
+        print("*** MinorViewController")
         print(#function)
     }
 }
