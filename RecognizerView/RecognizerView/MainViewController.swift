7a31b8792e4102f583f8c6288a7f862b7b2693d6
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-02-13 21:12:15 +0300

    Recognizer view

---
+++ RecognizerView/RecognizerView/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  RecognizerView
+//
+//  Created by Stanislav Lemeshaev on 13.02.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet private weak var customView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        // Создание распознавателя жестов
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        
+        // Настройка распознавателя жестов
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        
+        // Добавление распознавателя жестов к представлению
+        customView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    // Метод обработки жеста косания
+    @objc func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
+        view.backgroundColor = .systemCyan
+    }
+}
