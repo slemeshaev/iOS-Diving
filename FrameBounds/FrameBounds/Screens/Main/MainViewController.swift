ab70ac20abf06f350f3fede27b7f8dc3c7ecffa9
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-05-27 19:10:13 +0300

    Inticial settings

---
+++ FrameBounds/FrameBounds/Screens/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  FrameBounds
+//
+//  Created by Stanislav Lemeshaev on 27.05.2024.
+//  Copyright © 2024 slemeshaev. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
