d4c1686d578513334673c3461823dc92362887ed
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-05-27 20:24:31 +0300

    Added fill data to demostrate frame vs bounds

--- FrameBounds/FrameBounds/Screens/Main/MainViewController.swift
+++ FrameBounds/FrameBounds/Screens/Main/MainViewController.swift
@@ -9,8 +9,40 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet private weak var blueView: UIView!
+    @IBOutlet private weak var redView: UIView!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        fillData()
+    }
+    
+    // MARK: - Private
+    private func fillData() {
+        print("--- Blue View ---")
+        
+        print("Frame.origin.x: \(blueView.frame.origin.x)")
+        print("Frame.origin.y: \(blueView.frame.origin.y)")
+        print("Frame.size.width: \(blueView.frame.size.width)")
+        print("Frame.size.height: \(blueView.frame.size.height)")
+        
+        print("Bounds.origin.x: \(blueView.bounds.origin.x)")
+        print("Bounds.origin.y: \(blueView.bounds.origin.y)")
+        print("Bounds.size.width: \(blueView.bounds.size.width)")
+        print("Bounds.size.height: \(blueView.bounds.size.height)")
+        
+        print("--- Red View ---")
+        
+        print("Frame.origin.x: \(redView.frame.origin.x)")
+        print("Frame.origin.y: \(redView.frame.origin.y)")
+        print("Frame.size.width: \(redView.frame.size.width)")
+        print("Frame.size.height: \(redView.frame.size.height)")
+        
+        print("Bounds.origin.x: \(redView.bounds.origin.x)")
+        print("Bounds.origin.y: \(redView.bounds.origin.y)")
+        print("Bounds.size.width: \(redView.bounds.size.width)")
+        print("Bounds.size.height: \(redView.bounds.size.height)")
     }
 }
